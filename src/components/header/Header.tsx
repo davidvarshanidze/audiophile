8112643f3af0b0dc572b0214ecd8f068fbdba850
kafka <kafka@kafka.local>
2024-05-25 22:40:31 +0400

    initial commit

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <header>
+        
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">HEADPHONES</a></li>
+                    <li><a href="#">SPEAKERS</a></li>
+                    <li><a href="#">EARPHONES</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
