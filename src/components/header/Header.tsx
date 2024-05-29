55540a0b1fa8420e842a2370c92d01a10aa8e857
kafka <kafka@kafka.local>
2024-05-29 20:04:13 +0400

    added assets

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,9 @@ function Header() {
   return (
     <>
     <header>
-        
         <div>
             <nav>
-                <ul>
+                <ul className='flex gap-4 flex justify-center py-14'>
                     <li><a href="#">HOME</a></li>
                     <li><a href="#">HEADPHONES</a></li>
                     <li><a href="#">SPEAKERS</a></li>
