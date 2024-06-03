7e498a0364a257967bedf845a366d21c667c2a71
dvarshanidze <davittvarshanidze@gmail.com>
2024-06-03 21:54:31 +0400

    adding components

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "redux/types/reduxTypes";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  HeaderContainer,
+  HeaderWrap,
+  MobileNav,
+  MobileToggle,
+  MobileMenu,
+  HeaderLogoWrap,
+  HeaderLogo,
+  HeaderMenu,
+  MenuWrap,
+  MenuWrapItem,
+  CartWrap,
+  Cart,
+  CartNumber,
+} from "components/shared/header/headerStyles";
+
