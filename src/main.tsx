7f1a64cfd97d9dfab86c5db757bbac6400237396
dvarshanidze <davittvarshanidze@gmail.com>
2024-07-01 12:03:30 +0400

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { CartProvider } from './components/store/CartContextProvider'
 import './sass/index.scss'
+import { Analytics } from "@vercel/analytics/react"
 
 const stripeApiKey = import.meta.env.VITE_STRIPE_API_KEY;
 const coinbaseApiKey = import.meta.env.VITE_COINBASE_API_KEY;
@@ -12,6 +13,7 @@ console.log(stripeApiKey);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CartProvider>
+      <Analytics />
       <App />
     </CartProvider>
   </React.StrictMode>
